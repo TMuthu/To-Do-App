13845e61838f5ac965200bf9ce541d12a8bbdb21
TMuthu <tmuthukumarnpm@gmail.com>
2021-06-21 21:06:50 +0530

    Project files added

---
+++ index.js
@@ -0,0 +1,79 @@
+let itemArray = {};
+let indexNo = 1;
+
+let addItem = document.querySelector('.menu');
+addItem.addEventListener('submit', function addItems(e){
+    e.preventDefault();
+    let inputItemTag = document.querySelector('input.item');
+    let itemName = inputItemTag.value;
+    if(itemName === ''){
+        alert("Enter any value");
+    }
+    else{
+        itemArray[indexNo] = indexNo;
+        itemArray['itemName'] = itemName;
+        display(itemName,indexNo);
+        indexNo++;
+        inputItemTag.value='';
+    }
+});
+
+//add item to list container
+function display(itemName, indexNo){
+    let itemRow = document.createElement('div');
+    itemRow.classList.add('item-row');
+    //Creating Serial No Tag
+    let serialNoTag = document.createElement('div');
+    serialNoTag.classList.add('serialNo');
+    serialNoTag.innerText = indexNo;
+    itemRow.appendChild(serialNoTag);
+    //Creating Item Name Tag
+    let itemNameTag = document.createElement('div');
+    itemNameTag.classList.add('item-name');
+    itemNameTag.innerText = itemName;
+    itemRow.appendChild(itemNameTag);
+
+    let listcontainer = document.querySelector('.listcontainer');
+    listcontainer.appendChild(itemRow);
+    changeSerialNo();
+}
+
+function changeSerialNo(){
+    let serialNo_tag = document.querySelectorAll('.serialNo');
+    let sno = 1;
+    for(let s of serialNo_tag){
+        s.innerText = sno;
+        sno++;
+    }
+}
+
+
+
+let allItemRows = document.querySelector('.listcontainer');
+let popup = document.querySelector('.PopUp');
+let completebtn = document.querySelector('.Complete');
+let removebtn = document.querySelector('.remove');
+let canclebtn = document.querySelector('.cancle');
+
+allItemRows.addEventListener('click',function(e){
+    if(hasClass(e.target,'item-name')){
+        popup.style.display = 'block';
+        completebtn.addEventListener('click',function(){
+            e.target.style.textDecoration = 'line-through';
+            popup.style.display = 'none';
+        });
+        removebtn.addEventListener('click',function(){
+            e.target.parentNode.remove();
+            popup.style.display = 'none';
+            changeSerialNo();
+        });
+        canclebtn.addEventListener('click',function(){
+            popup.style.display = 'none';
+        });
+    }
+});
+    
+function hasClass(elem, className){
+    return elem.classList.contains(className);
+}
+
